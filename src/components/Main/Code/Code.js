075aaa7630e3900485e1705e82ea02a563909894
Younghyo Cho <yhcho0405@kakao.com>
2024-05-10 20:19:06 +0900

    feat: 코드 목록 업데이트

--- src/components/Main/Code/Code.js
+++ src/components/Main/Code/Code.js
@@ -7,21 +7,26 @@ import 'ace-builds/src-noconflict/theme-ambiance';
 import '../../Buttons/FileBtn'
 import FileBtn from "../../Buttons/FileBtn";
 
-export default function Code(){
-    const CodeEditorStyle = {width: '100%',height:'100%',border: '5px solid #3D3D3D', borderTop: '20px solid #3D3D3D'}
-    return(
-        <div className='code'>
-            <div style={{width: '100%',height:'25px', backgroundColor: 'black'}}>
-            <FileBtn></FileBtn>
-            </div>
-            <AceEditor
-                style ={CodeEditorStyle}
-                id="editor"
-                mode="javascript"
-                theme="ambiance"
-                name="code-editor"
-                editorProps={{ $blockScrolling: true }}
-            />
-        </div>
-    );
+export default function Code({ selectedCode }){
+  const CodeEditorStyle = {width: '100%',height:'100%',border: '5px solid #3D3D3D', borderTop: '20px solid #3D3D3D'}
+
+  return(
+    <div className='code'>
+      <div style={{width: '100%',height:'25px', backgroundColor: 'black'}}>
+        <FileBtn></FileBtn>
+      </div>
+      <AceEditor
+        style={CodeEditorStyle}
+        id="editor"
+        mode="javascript"
+        theme="ambiance"
+        name="code-editor"
+        value={selectedCode}
+        showPrintMargin={false}
+        editorProps={{ 
+            $blockScrolling: true,
+        }}
+      />
+    </div>
+  );
 }
--- src/components/Main/File/File.js
+++ src/components/Main/File/File.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import './File.css';
+import axios from 'axios';
 
-export default function File(){
-    return(
-        <div className='file'>
-        </div>
-    );
+export default function File({ setSelectedCode }) {
+  const [codeFiles, setCodeFiles] = useState([]);
+
+  useEffect(() => {
+    const fetchCodeFiles = async () => {
+        try {
+          const token = sessionStorage.getItem('accessToken');
+          const response = await axios.get('http://localhost:8080/api/v1/code-file', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setCodeFiles(response.data);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+  
+      fetchCodeFiles();
+  }, []);
+
+  const handleFileClick = (content) => {
+    setSelectedCode(content);
+  };
+
+  return (
+    <div className='file'>
+      <ul>
+        {codeFiles.map((codeFile) => (
+          <li key={codeFile.id} onClick={() => handleFileClick(codeFile.content)}>
+            <strong>File ID:</strong> {codeFile.id}<br />
+            {/* <strong>File Name:</strong> {codeFile.fileName}<br /> */}
+            {/* <strong>Content:</strong> {codeFile.content}<br /> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
