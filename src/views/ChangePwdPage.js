f1197a067dc2782841be7bd325577085fc914fc7
ezcho <ezcho886@naver.com>
2024-05-27 23:33:08 +0900

    feat: 해결사등장

--- src/views/ChangePwdPage.js
+++ src/views/ChangePwdPage.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Header from 'components/Header/Header';
+import "./ChangePwdPage.css";
 
-import "./ChangePwdPage.css"
 import ResetPassword from "components/Sign/ResetPassword";
 
 const btnTexts = require('lang/kor.json').login;
@@ -11,8 +11,8 @@ export default function ChangePwdPage(){
     const navigate = useNavigate();
     
     return(
-        <div className="container">
-            <Header></Header>
+        <div className='container'>
+        <Header />
             <div className="success-message" id="successMessage">{btnTexts[0]}</div>
             <ResetPassword />
             <div className="home">
