f0802bdfe50f15fbd738c04f364669131667b12d
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-06-16 13:25:56 +0900

    Use css file style only

--- src/components/Buttons/LBBtn.js
+++ src/components/Buttons/LBBtn.js
@@ -4,16 +4,6 @@ import './btns.css';
 const btnTexts = require('lang/kor.json').btns;
 
 export default function LBBtn({}){
-    const btnStyle = {
-        padding: "8px 16px",
-        fontSize: "12px",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-        backgroundColor: "#0a0f1c",
-        color:"white",
-        transition: "background-color 0.3s, box-shadow 0.3s"
-    }
     const navigate = useNavigate();
-    return <button btntype="leader" className="login-btn" style = {btnStyle} onClick={()=>{navigate("/leader");}}>리더보드</button>
+    return <button btntype="leader" className="leader-btn" onClick={()=>{navigate("/leader");}}>리더보드</button>
 }
