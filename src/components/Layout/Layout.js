aec1a638e88c3a60380499d6f805a46972a67363
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-05 04:05:37 +0900

    Fix starfield style bug

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import StarField from "components/Layout/StarField";
 
 export default function Layout(){
     return(
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%'}}>
+        <div style={{ display: 'flex', flexDirection: 'column', width:'100%', minHeight: '100%'}}>
             <StarField /> {/* 전체 앱에 StarField 배경을 추가합니다. */}
             <Header />
             <main style={{flex: 1, marginTop: "5%"}}>
