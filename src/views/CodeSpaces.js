9921c4b254e6a5cd5f40af3a7699bab562dda8c8
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-10-17 20:42:37 +0900

    Add some messages

--- src/views/CodeSpaces.js
+++ src/views/CodeSpaces.js
@@ -1,13 +1,35 @@
 import React from "react";
-import './CodeSpaces.css'
 import CodeSpace from 'components/CodeSpace/CodeSpace';
+import { TourProvider } from "@reactour/tour";
+import './CodeSpaces.css'
 
 export default function CodeSpaces() {
 
     return (
-        <div className='container'>
-            {/* CodeSpace 컴포넌트 */}
-            <CodeSpace></CodeSpace>
-        </div>
+        <TourProvider steps={steps}>
+            <div className='container'>
+                {/* CodeSpace 컴포넌트 */}
+                <CodeSpace></CodeSpace>
+            </div>
+        </TourProvider>
     );
 }
+
+const steps = [
+    {
+        content: 'Welcome to Codemos code space!',
+        position: 'center'
+    },
+    {
+        selector: '.colab-header .menu',
+        content: 'This is a menu for creating or loading files.',
+    },
+    {
+        selector: '.file-container',
+        content: 'This is your file list. All files you created will appear here.',
+    },
+    {
+        selector: '.code-container',
+        content: 'This is your code space. You can write your answer code for the problem you chose',
+    },
+]
