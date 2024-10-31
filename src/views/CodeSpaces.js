a2e1eab7dbb4657de13095ec649aa2164cf0923f
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-10-31 11:25:22 +0900

    Add more messages

--- src/views/CodeSpaces.js
+++ src/views/CodeSpaces.js
@@ -17,19 +17,27 @@ export default function CodeSpaces() {
 
 const steps = [
     {
-        content: 'Welcome to Codemos code space!',
+        content: 'Welcome to our Codemos Codespace!',
         position: 'center'
     },
     {
-        selector: '.colab-header .menu',
-        content: 'This is a menu for creating or loading files.',
+        selector: '.file-container .file',
+        content: 'This is your file list which contains all of your created code files. You can choose one of your file or create new one.',
     },
     {
-        selector: '.file-container',
-        content: 'This is your file list. All files you created will appear here.',
+        selector: '.colab-header .menu .file-buttons',
+        content: 'This is menu. You can create new file here.'
     },
     {
-        selector: '.code-container',
-        content: 'This is your code space. You can write your answer code for the problem you chose',
+        selector: '.container .space',
+        content: 'This space shows chosen problem and the codespace that contains your code.'
     },
+    {
+        selector: '.code .editor-container',
+        content: 'This is the codespace. You can freely write your code here.'
+    },
+    {
+        selector: '.colab-header .actions',
+        content: 'You can simulate your codes by clicking this run button. DOCS button will show the docs that contains all the information you will need.'
+    }
 ]
