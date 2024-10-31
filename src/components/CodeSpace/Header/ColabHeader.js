3978372b946267300ae7cde9e22b9003124166db
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-11-01 07:16:10 +0900

    Comment unused codes

--- src/components/CodeSpace/Header/ColabHeader.js
+++ src/components/CodeSpace/Header/ColabHeader.js
@@ -56,19 +56,19 @@ const ColabHeader = ({ toggleDocsVisibility, saveStatus }) => {
           onMouseOver={() => setDropdownVisible(true)}
           onMouseLeave={() => setDropdownVisible(false)}
         >
-          생성
+          New File
           {dropdownVisible && (
             <div className="dropdown-content">
               <button className="dropdown-item" onClick={handleNewJs}>New JavaScript File</button>
               <button className="dropdown-item" onClick={handleNewCPP}>New C File</button>
               <button className="dropdown-item" onClick={handleNewPython}>New Python File</button>
               <button className="dropdown-item" onClick={handleNewBlock}>New block coding File</button>
-              <button className="dropdown-item" onClick={handleOpenFile}>Open</button> {/* 파일 열기 버튼 */}
-              <button className="dropdown-item">Save</button>
+              {/*<button className="dropdown-item" onClick={handleOpenFile}>Open</button>
+              <button className="dropdown-item">Save</button>*/}
             </div>
           )}
         </div>
-        <div
+        {/*<div
           className="menu-button" 
           onMouseEnter={() => setEditDropdownVisible(true)}
           onMouseLeave={() => setEditDropdownVisible(false)}
@@ -96,15 +96,14 @@ const ColabHeader = ({ toggleDocsVisibility, saveStatus }) => {
             </div>
           )}
         </div>
-        <div className="menu-button">Tools</div>
-        <div className="menu-button">Help</div>
+        <div className="menu-button">Tools</div>*/}
+        <div className="menu-button help">Help</div>
       </div>
 
       <div className="actions">
       {saveStatus &&  <div className="action-button" style={{ color: 'green' ,fontSize:'80%' }}> {saveStatus}</div>}
       {judgeMessage && <div className="action-button" style={{ color: 'green' ,fontSize:'80%'}}>{judgeMessage}</div>}
-        <img src={runImg} onClick={runGame} alt="Run" />
-        <button className="action-button">remove</button>
+        <button className="action-button" onClick={runGame}><img src={runImg} alt="Run" /></button>
         <button className="action-button" onClick={toggleDocsVisibility}>DOCS</button>
         <button className="action-button"><span className="action-icon">&#9881;</span></button>
       </div>
